55565af0f2529cb0083b36203208ae548a51abf3
champnc <natdanai.dev@gmail.com>
2022-05-23 00:59:30 +0700

    add navbar

---
+++ frontend-react/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+const Navbar = () => {
+    const [showNav, setShowNav] = useState(false);
+
+    return (
+        <nav className="app__navbar flex justify-between items-center m-4">
+            <div className="app__navbar-logo text-3xl font-bold text-gray-800">
+                <h1>CHAMPNC</h1>
+            </div>
+            <ul className="app__navbar-links  flex justify-between items-center space-x-7">
+                {['About me', 'Experiance', 'Education', 'Work', 'Contact'].map((item) => (
+                    <li key={`link-${item}`} className="text-gray-400 hover:text-gray-900 duration-500">
+                        <div />
+                        <a href={`#${item.toLocaleLowerCase()}`} className="text-lg font-bold">{item}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
