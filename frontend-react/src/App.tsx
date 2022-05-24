a31f6c7149fecf133594332b9ff97afb1d8b852e
champnc <natdanai.dev@gmail.com>
2022-05-24 15:24:29 +0700

    change title

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
+import {Helmet} from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Champnc</title>
+        <meta name="description" content="champncfolio application" />
+      </Helmet>
       <Navbar/>
       <h1>Hello world</h1>
       <Footer/>
