1463e6124dac4d394ad301cb4d14bd884cf93b8b
champnc <natdanai.dev@gmail.com>
2022-05-25 21:21:30 +0700

    export contact section

--- frontend-react/src/pages/ContactSection/ContactSection.tsx
+++ frontend-react/src/pages/ContactSection/ContactSection.tsx
@@ -1,1 +1,10 @@
-export {}
+import React from "react"
+
+const ContactSection = () => {
+    return (
+        <div className="bg-gray-800">
+        </div>
+    )
+}
+
+export default ContactSection
--- frontend-react/src/pages/ContactSection/index.ts
+++ frontend-react/src/pages/ContactSection/index.ts
@@ -1,1 +1,1 @@
-export {}
+export { default as ContactSection } from "./ContactSection"
